c4113496a12d816c50d680c133957d07be798cfb
Brahmanya <98081415+brahmanyasudulagunta@users.noreply.github.com>
2026-02-12 19:32:52 -0600

    sqllite to postgres

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,11 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./data/users.db")
+DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://krishna:1729@postgres:5432/postgresdb")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
